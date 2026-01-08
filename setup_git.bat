da3b09c0c1a173d622a3eb9a7ddb9746db70ce17
johndoe <johndoe@example.com>
2026-01-08 12:04:06 +0530

    Update git setup script

--- setup_git.bat
+++ setup_git.bat
@@ -3,6 +3,6 @@ git init
 git add .
 git commit -m "Initial commit of TruthLens"
 git branch -M main
-git remote add origin https://github.com/Sujith2391/Fake_News_Check.git
+git remote add origin https://github.com/Sujith2391/Fake_News_Detection.git
 echo Git repository initialized and remote added.
 pause
